6881d42c02451529b4c84f2ce437edd62d1129e9
tojix-code <tojix46@gmail.com>
2026-03-25 14:47:12 +0530

    new

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -4,6 +4,7 @@ import { orderRouter } from "@/server/api/routers/order";
 import { paymentRouter } from "@/server/api/routers/payment";
 import { notificationRouter } from "@/server/api/routers/notification";
 import { profileRouter } from "@/server/api/routers/profile";
+import { postRouter } from "./routers/post";
 
 export const appRouter = createTRPCRouter({
   vendor: vendorRouter,
@@ -11,6 +12,7 @@ export const appRouter = createTRPCRouter({
   payment: paymentRouter,
   notification: notificationRouter,
   profile: profileRouter,
+  post: postRouter,
 });
 
 export type AppRouter = typeof appRouter;
